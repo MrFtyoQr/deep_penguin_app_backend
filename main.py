7bd468f37ea99dea45fe548938ee64b05b0d7331
MrFtyoQr <quintana.real.jqr@gmail.com>
2025-02-15 12:23:34 -0600

    DeepPenguin/Ultimate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from typing import Union
 
 from fastapi import FastAPI
-import uvicorn
 from api.study_guide import router as study_guide_router
 from api.healthcheck import router as healthcheck_router
 
@@ -11,6 +10,4 @@ app = FastAPI()
 app.include_router(study_guide_router)
 app.include_router(healthcheck_router)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0')
 
