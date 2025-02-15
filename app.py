f1a221be7ccfad189c6e99b1020391ace16860aa
MrFtyoQr <quintana.real.jqr@gmail.com>
2025-02-14 19:11:48 -0600

    DeepPenguin/v7

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from typing import Union
 
 from fastapi import FastAPI
 from api.study_guide import router as study_guide_router
+from api.healthcheck import router as healthcheck_router
 
 
 app = FastAPI()
 
 app.include_router(study_guide_router)
+app.include_router(healthcheck_router)
 
