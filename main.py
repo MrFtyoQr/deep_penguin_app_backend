90b31929449483b5cd458ab2019d1d1c9888e7a4
MrFtyoQr <quintana.real.jqr@gmail.com>
2025-03-15 12:46:01 -0600

    starlette

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Union
 from fastapi import FastAPI
 from api.study_guide import router as study_guide_router
 from api.healthcheck import router as healthcheck_router
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +11,14 @@ import os
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://deep-penguin.vercel.app/"],  # ⚠️ Cambia "*" por el dominio de tu frontend en producción
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos los headers
+)
 
 app.include_router(study_guide_router)
 app.include_router(healthcheck_router)
